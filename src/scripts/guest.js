031143942605ac8b7224d8b418baaaabd34e1ae4
clarkio <clarkio@users.noreply.github.com>
2018-07-30 00:05:15 -0400

    update guest overlay colors

---
+++ src/scripts/guest.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+  $('#container').append(
+    '<iframe src="https://streamelements.com/overlay/5b1ee08152ea3d895bfe9fac/3nzv5LKrgewVEDjcJFuJ" frameBorder="0" overflow="hidden" scrolling="no" seamless="seamless" />'
+  );
+  getCurrentBulbColor();
+});
