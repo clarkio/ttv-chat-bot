0c6318e770db7692426b604f4396ba761e2bb0c2
clarkio <clarkio@users.noreply.github.com>
2018-08-01 17:29:26 -0700

    add support for other effects on the overlay

--- src/scripts/stream.js
+++ src/scripts/stream.js
@@ -2,13 +2,43 @@ let STORE_OVERLAY_COLOR_NAME = 'streamOverlayColor';
 let beeDooAudio = new Audio('/assets/beedoo_minions.mp3');
 var socket = io();
 socket.on('color-effect', effect => {
-  if (effect.toLocaleLowerCase() === 'cop mode') {
+  const effectName = effect.toLocaleLowerCase();
+  if (effectName === 'cop mode') {
     triggerCopModeEffect();
+  } else if (effectName === 'subscribe') {
+    startSubscribeEffectOverlay();
+  } else if (effectName === 'follow') {
+    startFollowEffectOverlay();
   }
 });
+
+function startFollowEffectOverlay() {
+  startOverlayEffect('purple', 'white');
+}
+
+function startSubscribeEffectOverlay() {
+  startOverlayEffect('purple', 'green');
+}
+
 function triggerCopModeEffect() {
   startCopModeAudio();
-  startCopModeOverlay();
+  startOverlayEffect('cop-red', 'cop-blue');
+}
+
+function startOverlayEffect(startColor, endColor) {
+  let originalColor = localStorage.getItem(STORE_OVERLAY_COLOR_NAME);
+  let counter = 0;
+  let effectColor = startColor;
+  let overlayEffectInterval = setInterval(() => {
+    counter += 1;
+    if (counter === 20) {
+      setOverlayColor(originalColor);
+      clearInterval(overlayEffectInterval);
+    } else {
+      setOverlayColor(effectColor);
+      effectColor = effectColor === startColor ? endColor : startColor;
+    }
+  }, 500);
 }
 
 function startCopModeAudio() {
@@ -21,22 +51,6 @@ function startCopModeAudio() {
     minionDiv.toggleClass('copmode');
     beeDooAudio.pause();
   }, 10000);
-}
-
-function startCopModeOverlay() {
-  let originalColor = localStorage.getItem(STORE_OVERLAY_COLOR_NAME);
-  let counter = 0;
-  let copModeColor = 'red';
-  let overlayCopModeInterval = setInterval(() => {
-    counter += 1;
-    if (counter === 20) {
-      setOverlayColor(originalColor);
-      clearInterval(overlayCopModeInterval);
-    } else {
-      setOverlayColor(copModeColor);
-      copModeColor = copModeColor === 'red' ? 'blue' : 'red';
-    }
-  }, 500);
 }
 
 socket.on('color-change', color => {
