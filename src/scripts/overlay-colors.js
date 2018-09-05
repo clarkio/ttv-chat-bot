e68f6f1124e1e86c0a7ec9b70226e788ed023895
clarkio <clarkio@users.noreply.github.com>
2018-09-04 20:41:57 -0400

    WIP: customize overlay color

--- src/scripts/overlay-colors.js
+++ src/scripts/overlay-colors.js
@@ -1,6 +1,17 @@
 $(document).ready(() => {
+  let hueValue = 0;
   $('#get-overlay').click(() => {
     const url = $('#overlay-url').val();
     $('#overlay-frame').attr('src', url);
+  });
+
+  $('#increase-hue').click(() => {
+    hueValue = hueValue < 361 ? (hueValue += 1) : 360;
+    $('#container').css('filter', `hue-rotate(${hueValue}deg)`);
+  });
+
+  $('#decrease-hue').click(() => {
+    hueValue = hueValue > 0 ? (hueValue -= 1) : 0;
+    $('#container').css('filter', `hue-rotate(${hueValue}deg)`);
   });
 });
