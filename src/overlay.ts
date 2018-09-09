46a16d75596e0114223c986a713f1b75b21d33db
Nicholas March <n.march213@gmail.com>
2018-09-09 05:05:48 -0500

    doc overlay

--- src/overlay.ts
+++ src/overlay.ts
@@ -21,10 +21,16 @@ export class Overlay {
     //
   }
 
+  /**
+   * @returns The current Bulb Color
+   */
   public getCurrentColor = (): string => this.currentBulbColor;
 
-  public getSocket = (): SocketIO.Server => app.io;
-
+  /**
+   * Based on a specific events, trigger a special effect
+   *
+   * @param message - The message sent in chat
+   */
   public triggerSpecialEffect = (message: string): void => {
     let effect: string;
     if (message.includes('cop mode')) {
@@ -38,13 +44,20 @@ export class Overlay {
     } else {
       effect = 'follow';
     }
+    // communicate with the client to cause effect
     app.io.emit('color-effect', effect);
   };
 
+  /**
+   * Update the overlay to the given command
+   *
+   * @param command - What to change to overlay to
+   */
   public updateOverlay = (command: string): void => {
     this.supportedOverlayColors.forEach((color: string) => {
       if (command.includes(color)) {
         this.currentBulbColor = color;
+        // communicate with the client to change overlay color
         app.io.emit('color-change', color);
       }
     });
