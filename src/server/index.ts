5a82866ce8b4ec0892ce5a723cc2f8b243b5d45e
clarkio <clarkio@users.noreply.github.com>
2019-03-07 23:19:26 -0500

    wip: connecting to obs websocket server

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,11 +4,13 @@ import { AlertsManager } from './alerts-manager';
 import Overlay from './overlay';
 import EffectsManager from './effects-manager';
 import SoundFx from './sound-fx';
+import ObsManager from './obs-manager';
 import * as config from './config';
 
 const soundFx = new SoundFx();
 const overlay = new Overlay(soundFx);
 const appServer: AppServer = new AppServer(overlay);
+const obsManager = new ObsManager();
 
 const effectsManager = new EffectsManager();
 const alertManager: AlertsManager = new AlertsManager(
--- src/server/obs-manager.ts
+++ src/server/obs-manager.ts
@@ -1,13 +1,32 @@
-// tslint:disable-next-line: no-var-requires
-const ObsWebSocket = require('obs-websocket-js');
+import * as config from './config';
+
+import ObsWebSocket from 'obs-websocket-js';
 
 export default class ObsManager {
-  obs: any;
+  public sceneList: any;
+  private obs: ObsWebSocket;
+
   constructor() {
     this.obs = new ObsWebSocket();
-    this.obs.connect({
-      address: 'localhost:4444',
-      password: '$up3rSecretP@ssw0rd'
+    this.obs
+      .connect({
+        address: config.obsSocketsServer,
+        password: config.obsSocketsKey
+      })
+      .then(() => {
+        this.getSceneList();
+      });
+    this.obs.on('error', this.handleError);
+  }
+
+  private handleError(error: any): any {
+    console.error(error);
+  }
+
+  private getSceneList() {
+    this.obs.send('GetSceneList').then((data: any) => {
+      console.log('Scenes Found:', data.scenes);
+      this.sceneList = data.scenes;
     });
   }
 }
