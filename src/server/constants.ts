f1497ba68fd7fc536a94243d228c0f6f5e32ca06
clarkio <clarkio@users.noreply.github.com>
2019-12-13 14:55:54 -0500

    add refactor for index

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -26,3 +26,10 @@ export const effectsManager = {
     'the sound effect you entered is not supported. Please double check your spelling or use the !sfx command to see what is supported',
   failedSoundEffectMessage: 'failed to play the sorry sound effect'
 };
+
+export const index = {
+  logs: {
+    configFileReadWarningMessage:
+      'Unable to retrieve configuration from a file. Falling back to environment variables'
+  }
+};
