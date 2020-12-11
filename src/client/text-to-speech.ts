c7b6341364d09c0bfc141c8ee455a808911bdb30
clarkio <clarkio@users.noreply.github.com>
2020-12-11 16:41:25 -0500

    wip: it works consistently now

--- src/client/text-to-speech.ts
+++ src/client/text-to-speech.ts
@@ -8,7 +8,7 @@ const SpeechSDK = window.SpeechSDK;
 SpeechSDK.Recognizer.enableTelemetry(false);
 let speechConfig: any;
 let synthesizer: any;
-const audioConfig = SpeechSDK.AudioConfig.fromDefaultSpeakerOutput();
+let azureSpeechToken = '';
 
 const requestOptions = {
   contentType: 'application/json',
@@ -17,21 +17,11 @@ const requestOptions = {
 };
 request(requestOptions).done(result => {
   console.log(result);
-  speechConfig = SpeechSDK.SpeechConfig.fromSubscription(result, "westus2");
-  speechConfig.speechSynthesisVoiceName = 'en-IE-EmilyNeural';
-  synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig, audioConfig);
+  azureSpeechToken = result;
 });
 
 let speechQueue: string[] = [];
 let isSpeaking: boolean = false;
-
-setupSpeechToText();
-
-function setupSpeechToText() {
-  // utterance.voice = synth.getVoices()[9];
-  // utterance.onend = finishUtterance;
-  // utterance.onstart = startUtterance;
-}
 
 socket.on('tts', (textToSpeak: string) => {
   addToSpeakQueue(textToSpeak);
@@ -47,37 +37,41 @@ function addToSpeakQueue(textToSpeak: string) {
   speechQueue.push(textToSpeak);
 }
 
+function finishUtterance(): any {
+  isSpeaking = false;
+  speak();
+}
+
+function startUtterance() {
+  isSpeaking = true;
+}
+
 function speak() {
   if (speechQueue.length > 0 && !isSpeaking) {
-    // utterance.text = speechQueue.shift() as string;
-    // synth.speak(utterance);
+    let speechConfig = SpeechSDK.SpeechConfig.fromSubscription(azureSpeechToken, "westus2");
+    startUtterance();
+    speechConfig.speechSynthesisVoiceName = 'en-IE-EmilyNeural';
+    synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig);
     synthesizer.speakTextAsync(
         speechQueue.shift() as string,
         (result:any) => {
             if (result) {
                 console.log(JSON.stringify(result));
             }
+            synthesizer.close();
+            synthesizer = undefined;
         },
         (error:any) => {
             console.error(error);
             synthesizer.close();
+            synthesizer = undefined;
         });
   }
 }
 
-function finishUtterance(event: Event): any {
-  isSpeaking = false;
-  speak();
-}
-
-function startUtterance(event: Event) {
-  isSpeaking = true;
-}
-
 function stopSpeaking() {
-  // synth.cancel();
   synthesizer.close();
-  synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig, audioConfig);
+  synthesizer = undefined;
   isSpeaking = false;
 }
 
