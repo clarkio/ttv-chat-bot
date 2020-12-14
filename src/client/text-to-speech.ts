d62f4c0dc7e6105e29a919ca30db5a3c62adbbfd
clarkio <clarkio@users.noreply.github.com>
2020-12-13 23:24:31 -0500

    working solution with queuing and skip

--- src/client/text-to-speech.ts
+++ src/client/text-to-speech.ts
@@ -2,7 +2,6 @@
 const SpeechSDK = window.SpeechSDK;
 // @ts-ignore
 const axios = window.axios;
-// import { SpeechSynthesisResult } from 'microsoft-cognitiveservices-speech-sdk';
 SpeechSDK.Recognizer.enableTelemetry(false);
 let speechConfig: any;
 let synthesizer: any;
@@ -15,14 +14,15 @@ let audioCtx = new AudioContext({
   sampleRate: 48000,
 });
 let queue: AudioBuffer[] = [];
-let currentAudio = null;
+let currentAudio: AudioBufferSourceNode;
 let isPlaying: boolean = false;
 let speechQueue: string[] = [];
 let isSpeaking: boolean = false;
+const resetTokenTimeInMinutes = 9
+let resetTokenInterval;
 
 axios.get('/tokens?name=azureSpeechToken')
   .then((result:any) => {
-    console.log(result);
     azureSpeechToken = result.data;
     getAzureSpeechApiToken();
   })
@@ -31,7 +31,6 @@ axios.get('/tokens?name=azureSpeechToken')
   })
 
 function getAzureSpeechApiToken() {
-  // TODO: get a new token after 9 minutes
   // Token is only valid for 10 minutes:
   // https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-text-to-speech#how-to-use-an-access-token
 
@@ -41,8 +40,8 @@ function getAzureSpeechApiToken() {
     }
   })
   .then((result:any) => {
-    console.log(result);
     azureSpeechApiAccessToken = result.data;
+    resetTokenInterval = setInterval(getAzureSpeechApiToken, resetTokenTimeInMinutes * 60 * 1000);
   })
   .catch((error: any) => {
     console.error(error);
@@ -63,90 +62,55 @@ function addToSpeakQueue(textToSpeak: string): void {
   speechQueue.push(textToSpeak);
 }
 
-function finishUtterance(): void {
+function finishSpeaking(): void {
   isSpeaking = false;
   loadText();
 }
 
-function startUtterance(): void {
-  isSpeaking = true;
-}
-
+// TODO: restructure to the following
 //  function speak()
 //    calls async loadText
 //    calls async playText from loadText result
 
 function loadText() {
-  const textToSpeak = speechQueue.shift() as string;
-
-  const body = `
-  <speak version='1.0' xml:lang='en-US'>
-    <voice xml:lang='en-US' xml:gender='Female' name='en-US-AriaRUS'>
-      ${textToSpeak}
-    </voice>
-  </speak>`;
-
-  axios.post(azureTextToSpeechUrl, body, {
-    headers: {
-      'Authorization': `Bearer ${azureSpeechApiAccessToken}`,
-      'X-Microsoft-OutputFormat': 'audio-24khz-48kbitrate-mono-mp3',
-      'Content-Type': 'application/ssml+xml'
-    }
-  })
-  .then(async (result: any) => {
+  if (speechQueue.length > 0 && !isSpeaking) {
     isSpeaking = true;
-    console.log(result);
+    const textToSpeak = speechQueue.shift() as string;
 
-    const view = new TextEncoder().encode(result.data).buffer as ArrayBuffer;
-    const audioBuffer = await audioCtx.decodeAudioData(view);
-    const audioSource = audioCtx.createBufferSource();
-    audioSource.buffer = audioBuffer;
-    audioSource.connect(audioCtx.destination);
-    currentAudio = audioSource;
-    audioSource.start(audioCtx.currentTime);
-    audioSource.onended = () => {
-      currentAudio = null;
+    const body = `
+    <speak version='1.0' xml:lang='en-US'>
+      <voice xml:lang='en-US' xml:gender='Female' name='en-US-AriaRUS'>
+        ${textToSpeak}
+      </voice>
+    </speak>`;
+
+    axios.post(azureTextToSpeechUrl, body, {
+      headers: {
+        'Authorization': `Bearer ${azureSpeechApiAccessToken}`,
+        'X-Microsoft-OutputFormat': 'audio-16khz-64kbitrate-mono-mp3',
+        'Content-Type': 'application/ssml+xml'
+      },
+      responseType: 'arraybuffer'
+    })
+    .then(async (result: any) => {
+      const audioBuffer = await audioCtx.decodeAudioData(result.data);
+      const audioSource = audioCtx.createBufferSource();
+      audioSource.buffer = audioBuffer;
+      audioSource.connect(audioCtx.destination);
+      currentAudio = audioSource;
+      audioSource.start(0);
+      audioSource.onended = function finishedPlayingAudio (ev: Event) {
+        finishSpeaking();
+      };
+    })
+    .catch((error:any) => {
+      console.error(error);
       isSpeaking = false;
-      return;
-    }
-  })
-  .catch((error:any) => {
-    console.error(error);
-  });
+    });
+  }
 }
 
 function stopSpeaking() {
-  synthesizer.close();
-  synthesizer = undefined;
+  currentAudio.stop();
   isSpeaking = false;
 }
-
-/*
-function speak() {
-  if (speechQueue.length > 0 && !isSpeaking) {
-    startUtterance();
-    let speechConfig = SpeechSDK.SpeechConfig.fromSubscription(azureSpeechToken, "westus2");
-    speechConfig.speechSynthesisVoiceName = 'en-IE-EmilyNeural';
-    synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig);
-    const textToSpeak = speechQueue.shift() as String;
-    synthesizer.speakTextAsync(textToSpeak, synthesizerCompleted.bind(null, textToSpeak) as any, synthesizerFailed);
-  }
-}
-
-function synthesizerCompleted (textToSpeak: String, result: SpeechSynthesisResult) {
-  let logMessage = 'Speaking has completed: '
-  if (result.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
-    logMessage += "synthesis finished for [" + textToSpeak + "].\n";
-    resetSpeaking();
-  } else if (result.reason === SpeechSDK.ResultReason.Canceled) {
-    logMessage += "synthesis failed. Error detail: " + result.errorDetails + "\n";
-    resetSpeaking();
-  }
-  console.log(logMessage);
-}
-
-function synthesizerFailed (error: any) {
-  console.error(error);
-  resetSpeaking();
-}
-*/
