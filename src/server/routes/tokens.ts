288f01f15a78cdefb9a6336f0c32b309fe08cb0c
clarkio <clarkio@users.noreply.github.com>
2021-09-29 14:25:24 -0400

    fix: limit env variables retrieved from the api

--- src/server/routes/tokens.ts
+++ src/server/routes/tokens.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import * as config from '../config';
 
-// TODO: limit to only certain environment variables
 export const tokensRoute = (req: express.Request, res: express.Response) => {
   const { name } = req.query;
-  if (name && config.tokens[name]) {
+  if (name && isSupported(name) && config.tokens[name]) {
     res.send(config.tokens[name]);
   } else {
     res.statusCode = 400;
     res.send('Not a valid token');
   }
 };
+
+function isSupported(name: string): boolean {
+  // Only support the one environment variable
+  return name === 'azureSpeechToken';
+}
 
 // phrakberg was here too
